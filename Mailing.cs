d6b86998faa813462f07060ef48bd05ee379132f
Melissa Wong <mellymai@gmail.com>
2018-01-10 12:00:49 -0800

    First Look

---
+++ Mailing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstLook.Models
+{
+    public class Mailing
+    {
+        public Mailing()
+        {
+        }
+    }
+}
